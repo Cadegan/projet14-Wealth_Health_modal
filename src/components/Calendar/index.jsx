3b32444508dbe7a47beff118a5c240db3e27a705
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-11-22 07:21:07 +0100

    userAction

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,27 +1,52 @@
 import * as React from "react";
 import { Controller } from "react-hook-form";
-import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+// import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import DateFnsUtils from "@date-io/date-fns";
 import {
-  MuiPickersUtilsProvider,
   KeyboardDatePicker,
+  MuiPickersUtilsProvider,
 } from "@material-ui/pickers";
 
-export default function Calendar({ control }) {
-  <MuiPickersUtilsProvider utils={AdapterMoment}>
-    <Controller
-      name="MUIPicker"
-      control={control}
-      render={({ field: { ref, ...rest } }) => (
-        <KeyboardDatePicker
-          margin="normal"
-          id="date-picker-dialog"
-          format="MM/DD/YYYY"
-          KeyboardButtonProps={{
-            "aria-label": "change date",
-          }}
-          {...rest}
-        ></KeyboardDatePicker>
-      )}
-    ></Controller>
-  </MuiPickersUtilsProvider>;
+export default function Agenda({ control }) {
+  // <MuiPickersUtilsProvider utils={AdapterMoment}>
+  //   <Controller
+  //     name="MUIPicker"
+  //     control={control}
+  //     render={({ field: { ref, ...rest } }) => (
+  //       <KeyboardDatePicker
+  //         margin="normal"
+  //         id="date-picker-dialog"
+  //         format="MM/DD/YYYY"
+  //         KeyboardButtonProps={{
+  //           "aria-label": "change date",
+  //         }}
+  //         {...rest}
+  //       ></KeyboardDatePicker>
+  //     )}
+  //   ></Controller>
+  // </MuiPickersUtilsProvider>;
+
+  <div className="container">
+    <section>
+      <label>MUI Picker</label>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Controller
+          name="MUIPicker"
+          control={control}
+          render={({ field: { ref, ...reste } }) => (
+            <KeyboardDatePicker
+              margin="normal"
+              id="date-picker-dialog"
+              label="Date picker dialog"
+              format="MM/dd/yyyy"
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+              {...reste}
+            />
+          )}
+        />
+      </MuiPickersUtilsProvider>
+    </section>
+  </div>;
 }
