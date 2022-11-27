335f169e3abafd099fd38522cad7e37e7fd0e9fa
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-11-27 05:56:07 +0100

    Name - Pattern working with bug message 🪳

--- src/pages/Home/test-form.jsx
+++ src/pages/Home/test-form.jsx
@@ -1,20 +1,164 @@
+// import { Controller, useForm } from "react-hook-form";
+// import { useState, useEffect } from "react";
+// import Calendar from "../../components/Calendar";
+// // import { TextField } from "@material-ui/core";
+// import { TextField, Grid, Box, Button } from "@mui/material";
+
+// export default function Home() {
+//   const { control, handleSubmit } = useForm({
+//     reValidateMode: "onBlur",
+//   });
+
+//   const myHelper = {
+//     input: {
+//       required: "Email is Required",
+//       pattern: "Invalid Email Address",
+//     },
+//   };
+
+//   const [data, setData] = useState(null);
+
+//   // const [value, setValue] = useState("");
+//   // const [valid, setValid] = useState(true);
+
+//   // const handleValidation = (e) => {
+//   //   setValue(e.target.value);
+//   //   const reg = new RegExp("[a-z]");
+//   //   setValid(reg.test(e.target.value));
+//   //   console.log("isValid?", reg.test(e.target.value));
+//   // };
+
+//   const handleOnSubmit = (evt) => {
+//     console.log(evt);
+//   };
+
+//   useEffect(() => {
+//     /* This is a ternary operator. It is saying if there is something in localStorage, then parse it
+//     and set it to employees. If there is nothing in localStorage, then set employees to an empty
+//     array. */
+//     const employees = JSON.parse(localStorage.getItem("employees")) || [];
+//     employees.push(data);
+//     localStorage.setItem("employees", JSON.stringify(employees));
+//   });
+
+//   return (
+//     <div className="App">
+//       <Box component="form" onSubmit={handleSubmit(handleOnSubmit)}>
+//         <Grid container spacing={3}>
+//           <Grid item xs={6}>
+//             <Controller
+//               control={control}
+//               name="email"
+//               defaultValue=""
+//               // rules={{
+//               //   required: true,
+//               //   pattern: "[a-z]{3,15}",
+//               // }}
+//               rules={{
+//                 maxLength: {
+//                   value: 15,
+//                   message: "Must be less than 6 char",
+//                 },
+//                 minLength: {
+//                   value: 3,
+//                   // message: "Must be greater 1 char",
+//                 },
+//                 required: {
+//                   // message: "Required",
+//                   value: true,
+//                 },
+//               }}
+//               render={({ field, fieldState: { errors } }) => (
+//                 <TextField
+//                   {...field}
+//                   type="text"
+//                   fullWidth
+//                   label="Email With Validation"
+//                   error={errors !== undefined}
+//                   helperText={errors ? myHelper.input[errors.type] : ""}
+//                   // rules={{
+//                   //   required: true,
+//                   //   pattern: "[a-z]{3,15}",
+//                   // }}
+//                   /** */
+//                   inputProps={{
+//                     pattern: "[a-z]{3,15}",
+//                     required: true,
+//                   }}
+//                 />
+//               )}
+//             />
+//           </Grid>
+//           {/* <Grid item xs={12}>
+//             {members.map((field, index) => (
+//               <Grid container key={field.id} spacing={1} alignItems="center">
+//                 <Grid item xs={6}>
+//                   <Controller
+//                     control={control}
+//                     // must use . for the object key!!!
+//                     name={`members.${index}.email`}
+//                     defaultValue=""
+//                     render={({ field }) => (
+//                       <TextField {...field} type="email" fullWidth />
+//                     )}
+//                   />
+//                 </Grid>
+//                 <Grid item xs={4}>
+//                   <Controller
+//                     control={control}
+//                     // must use . for the object key!!!
+//                     name={`members.${index}.role`}
+//                     defaultValue="user"
+//                     render={({ field }) => (
+//                       <Select {...field} fullWidth>
+//                         <MenuItem value="user">Member</MenuItem>
+//                         <MenuItem value="admin">Admin</MenuItem>
+//                       </Select>
+//                     )}
+//                   />
+//                 </Grid>
+//                 <Grid item xs={2}>
+//                   <Button
+//                     color="error"
+//                     variant="text"
+//                     onClick={() => removeMemberRow(index)}
+//                   >
+//                     Delete
+//                   </Button>
+//                 </Grid>
+//               </Grid>
+//             ))}
+//             <Button variant="contained" onClick={addNewMemeber}>
+//               Add
+//             </Button>
+//           </Grid> */}
+//           <Grid item xs={12}>
+//             <Button type="submit">Submit</Button>
+//           </Grid>
+//         </Grid>
+//       </Box>
+//     </div>
+//   );
+// }
+
 import { Controller, useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
 import Calendar from "../../components/Calendar";
 // import { TextField } from "@material-ui/core";
-import { TextField, Grid, Box, Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
 export default function Home() {
-  const { control, handleSubmit } = useForm({
-    reValidateMode: "onBlur",
+  const {
+    // register,
+    handleSubmit,
+    control,
+    // formState: { errors },
+  } = useForm({
+    criteriaMode: "all",
+    mode: "onChange",
   });
 
-  const myHelper = {
-    input: {
-      required: "Email is Required",
-      pattern: "Invalid Email Address",
-    },
-  };
+  // console.log("errors", errors);
 
   const [data, setData] = useState(null);
 
@@ -23,14 +167,10 @@ export default function Home() {
 
   // const handleValidation = (e) => {
   //   setValue(e.target.value);
-  //   const reg = new RegExp("[a-z]");
+  //   const reg = new RegExp("[A-Za-z]");
   //   setValid(reg.test(e.target.value));
   //   console.log("isValid?", reg.test(e.target.value));
   // };
-
-  const handleOnSubmit = (evt) => {
-    console.log(evt);
-  };
 
   useEffect(() => {
     /* This is a ternary operator. It is saying if there is something in localStorage, then parse it
@@ -42,101 +182,158 @@ export default function Home() {
   });
 
   return (
-    <div className="App">
-      <Box component="form" onSubmit={handleSubmit(handleOnSubmit)}>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
+    <main className="main">
+      <div className="title">
+        <h1>HRnet</h1>
+      </div>
+      <div className="container">
+        <a href="employee-list.html">View Current Employees</a>
+        <h2>Create Employee</h2>
+        <form onSubmit={handleSubmit(setData)}>
+          {/**
+           * First Name Input
+           */}
+          <section className="firstNameSection">
             <Controller
+              name="firstName"
               control={control}
-              name="email"
-              defaultValue=""
-              // rules={{
-              //   required: true,
-              //   pattern: "[a-z]{3,15}",
-              // }}
               rules={{
-                maxLength: {
-                  value: 15,
-                  message: "Must be less than 6 char",
+                required: {
+                  message: "Required",
+                  value: true,
+                  // pattern: "[A-Za-z]",
+                },
+                pattern: {
+                  value: /[A-Za-z]/,
+                  message: "Please no characters",
                 },
                 minLength: {
                   value: 3,
-                  // message: "Must be greater 1 char",
+                  message: "Please enter at least 3 characters",
                 },
-                required: {
-                  // message: "Required",
-                  value: true,
+                maxLength: {
+                  value: 15,
+                  message: "Please enter less than 15 characters",
                 },
               }}
-              render={({ field, fieldState: { errors } }) => (
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
                 <TextField
-                  {...field}
-                  type="text"
-                  fullWidth
-                  label="Email With Validation"
-                  error={errors !== undefined}
-                  helperText={errors ? myHelper.input[errors.type] : ""}
-                  // rules={{
-                  //   required: true,
-                  //   pattern: "[a-z]{3,15}",
-                  // }}
-                  /** */
+                  required={true}
+                  value={value}
+                  id="firstName"
+                  label="First name"
+                  onChange={onChange}
                   inputProps={{
-                    pattern: "[a-z]{3,15}",
-                    required: true,
+                    pattern: "[A-Za-z]{3,15}",
                   }}
+                  error={!!error}
+                  helperText={error ? error.message : null}
                 />
               )}
             />
-          </Grid>
-          {/* <Grid item xs={12}>
-            {members.map((field, index) => (
-              <Grid container key={field.id} spacing={1} alignItems="center">
-                <Grid item xs={6}>
-                  <Controller
-                    control={control}
-                    // must use . for the object key!!!
-                    name={`members.${index}.email`}
-                    defaultValue=""
-                    render={({ field }) => (
-                      <TextField {...field} type="email" fullWidth />
-                    )}
+          </section>
+
+          {/**
+           * Last Name Input
+           */}
+          <section className="lastNameSection">
+            <Controller
+              name="lastName"
+              control={control}
+              rules={{
+                required: {
+                  message: "Required",
+                  value: true,
+                },
+                minLength: {
+                  value: 3,
+                  message: "Please enter at least 3 characters",
+                },
+                maxLength: {
+                  value: 15,
+                  message: "Please enter less than 15 characters",
+                },
+              }}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  required={true}
+                  id="lastName"
+                  label="Last name"
+                  value={value}
+                  onChange={onChange}
+                  inputProps={{
+                    pattern: "[A-Za-z]{3,15}",
+                  }}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                />
+              )}
+            />
+          </section>
+
+          {/* 
+
+          <section className="birthDaySection">
+            <Calendar control={control}></Calendar>
+          </section>
+
+          <section className="addressSection">
+            <div className="street">
+              <Controller
+                rules={{ required: true, minLength: 1 }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    required
+                    id="streetAddress"
+                    name="streetAddress"
+                    label="Street"
+                    autoComplete="street-address"
                   />
-                </Grid>
-                <Grid item xs={4}>
-                  <Controller
-                    control={control}
-                    // must use . for the object key!!!
-                    name={`members.${index}.role`}
-                    defaultValue="user"
-                    render={({ field }) => (
-                      <Select {...field} fullWidth>
-                        <MenuItem value="user">Member</MenuItem>
-                        <MenuItem value="admin">Admin</MenuItem>
-                      </Select>
-                    )}
+                )}
+                name="streetAddress"
+                control={control}
+              />
+              {errors.streetAddress && (
+                <span className="text-error">required</span>
+              )}
+            </div>
+          </section>
+
+          <section className="citySection">
+            <div className="city">
+              <Controller
+                rules={{ required: true, minLength: 1 }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    required
+                    id="cityAddress"
+                    name="cityAddress"
+                    label="City"
+                    autoComplete="city-address"
                   />
-                </Grid>
-                <Grid item xs={2}>
-                  <Button
-                    color="error"
-                    variant="text"
-                    onClick={() => removeMemberRow(index)}
-                  >
-                    Delete
-                  </Button>
-                </Grid>
-              </Grid>
-            ))}
-            <Button variant="contained" onClick={addNewMemeber}>
-              Add
-            </Button>
-          </Grid> */}
-          <Grid item xs={12}>
-            <Button type="submit">Submit</Button>
-          </Grid>
-        </Grid>
-      </Box>
-    </div>
+                )}
+                name="cityAddress"
+                control={control}
+              />
+              {errors.cityAddress && (
+                <span className="text-error">required</span>
+              )}
+            </div>
+          </section> */}
+
+          <button className="button" type="submit">
+            Save
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
