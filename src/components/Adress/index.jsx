dc3bf9f31caef1e455b04afc07ebf6be3ec98463
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-11-28 20:18:55 +0100

    Adress : Revision

---
+++ src/components/Adress/index.jsx
@@ -0,0 +1,56 @@
+import { Controller } from "react-hook-form";
+
+import TextField from "@mui/material/TextField";
+
+const Adress = ({
+  control,
+  controllerPattern,
+  inputPropsPattern,
+  minLength,
+  maxLength,
+  name,
+  label,
+}) => {
+  return (
+    <div className="street">
+      <Controller
+        name={`adress.${name}`}
+        control={control}
+        rules={{
+          required: {
+            message: "Required",
+            value: true,
+          },
+          pattern: {
+            value: controllerPattern,
+            message: "Special characters not allowed",
+          },
+          minLength: {
+            value: minLength,
+            message: `Please enter at least ${minLength} characters`,
+          },
+          maxLength: {
+            value: maxLength,
+            message: `Please enter less than ${maxLength} characters`,
+          },
+        }}
+        render={({ field: { onChange, value }, fieldState: { error } }) => (
+          <TextField
+            required={true}
+            value={value}
+            id={name}
+            label={label}
+            onChange={onChange}
+            inputProps={{
+              pattern: inputPropsPattern,
+            }}
+            error={!!error}
+            helperText={error ? error.message : null}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+export default Adress;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
 import Calendar from "../../components/Calendar";
 import State from "../../components/State";
 // import { TextField } from "@material-ui/core";
-import TextField from "@mui/material/TextField";
+// import TextField from "@mui/material/TextField";
 // import { Autocomplete } from "@mui/material";
 import IdentityInput from "../../components/Identity";
+import Adress from "../../components/Adress";
 
 export default function Home() {
   const {
@@ -56,160 +57,49 @@ export default function Home() {
             label="First name"
           />
           <IdentityInput control={control} name="lastName" label="Last name" />
-
           <Calendar control={control} />
 
           <section className="startDateSection">
             <p>Start Date</p>
           </section>
 
-          {/**
-           * Adress section
-           */}
           <section className="adressSection">
             <p>Adress</p>
-            {/**
-             * Street Input
-             */}
-            <div className="street">
-              <Controller
-                name="adress.street"
-                control={control}
-                rules={{
-                  required: {
-                    message: "Required",
-                    value: true,
-                  },
-                  pattern: {
-                    value: /^[_A-z0-9_ -]*((-|\s)*[_A-z0-9_ -])*$/g,
-                    message: "Special characters not allowed",
-                  },
-                  minLength: {
-                    value: 3,
-                    message: "Please enter at least 3 characters",
-                  },
-                  maxLength: {
-                    value: 30,
-                    message: "Please enter less than 30 characters",
-                  },
-                }}
-                render={({
-                  field: { onChange, value },
-                  fieldState: { error },
-                }) => (
-                  <TextField
-                    required={true}
-                    value={value}
-                    id="streetName"
-                    label="Street"
-                    onChange={onChange}
-                    inputProps={{
-                      pattern: "[A-Za-z0-9_ -]{3,30}",
-                    }}
-                    error={!!error}
-                    helperText={error ? error.message : null}
-                  />
-                )}
-              />
-            </div>
 
-            {/**
-             * City Input
-             */}
-            <div className="city">
-              <Controller
-                name="adress.city"
-                control={control}
-                rules={{
-                  required: {
-                    message: "Required",
-                    value: true,
-                  },
-                  pattern: {
-                    value: /^[_A-z_ -]*((-|\s)*[_A-z_ -])*$/g,
-                    message: "Special characters not allowed",
-                  },
-                  minLength: {
-                    value: 3,
-                    message: "Please enter at least 3 characters",
-                  },
-                  maxLength: {
-                    value: 30,
-                    message: "Please enter less than 30 characters",
-                  },
-                }}
-                render={({
-                  field: { onChange, value },
-                  fieldState: { error },
-                }) => (
-                  <TextField
-                    required={true}
-                    value={value}
-                    id="cityName"
-                    label="City"
-                    onChange={onChange}
-                    inputProps={{
-                      pattern: "[A-Za-z_ -]{3,30}",
-                    }}
-                    error={!!error}
-                    helperText={error ? error.message : null}
-                  />
-                )}
-              />
-            </div>
+            <Adress
+              control={control}
+              controllerPattern={/^[_A-z0-9_ -]*((-|\s)*[_A-z0-9_ -])*$/g}
+              inputPropsPattern="[A-Za-z0-9_ -]{3,30}"
+              minLength="3"
+              maxLength="30"
+              name="street"
+              label="Street"
+            />
 
-            {/**
-             * State Input
-             */}
+            <Adress
+              control={control}
+              controllerPattern={/^[_A-z_ -]*((-|\s)*[_A-z_ -])*$/g}
+              inputPropsPattern="[A-Za-z_ -]{3,30}"
+              minLength="3"
+              maxLength="30"
+              name="city"
+              label="City"
+            />
+
             <div className="state">
               <State control={control}></State>
             </div>
           </section>
 
-          {/**
-           * Zip Code Input
-           */}
-          <div className="zipCode">
-            <Controller
-              name="adress.zipCode"
-              control={control}
-              rules={{
-                required: {
-                  message: "Required",
-                  value: true,
-                },
-                pattern: {
-                  value: /^[_0-9_ -]*((-|\s)*[_0-9_ -])*$/g,
-                  message: "Not allowed",
-                },
-                minLength: {
-                  value: 4,
-                  message: "Please enter at least 4 characters",
-                },
-                maxLength: {
-                  value: 9,
-                  message: "Please enter less than 9 characters",
-                },
-              }}
-              render={({
-                field: { onChange, value },
-                fieldState: { error },
-              }) => (
-                <TextField
-                  required={true}
-                  value={value}
-                  id="zipCode"
-                  label="Zip Code"
-                  onChange={onChange}
-                  inputProps={{
-                    pattern: "[0-9_ -]{4,9}",
-                  }}
-                  error={!!error}
-                  helperText={error ? error.message : null}
-                />
-              )}
-            />
-          </div>
+          <Adress
+            control={control}
+            controllerPattern={/^[_0-9_ -]*((-|\s)*[_0-9_ -])*$/g}
+            inputPropsPattern="[0-9_ -]{4,9}"
+            minLength="4"
+            maxLength="9"
+            name="zipCode"
+            label="Zip Code"
+          />
 
           <button className="button" type="submit">
             Save
