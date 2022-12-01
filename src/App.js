83d9f3019402714735c3d6baf1f35b2cbae53223
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-12-01 03:08:18 +0100

    Updates dependencies ⚡️

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 // import MainNav from "./components/MainNav";
 // import Footer from "./components/Footer";
 import Home from "./pages/Home";
 // import EmployeeList from "./pages/EmployeeList";
 import Error from "./pages/Error";
 
-function App() {
- 
+const theme = createMuiTheme();
 
+function App() {
   return (
-    <Router>
-      {/* <MainNav /> */}
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        {/* <Route exact path="/employee-list" element={<EmployeeList />}></Route> */}
-        <Route exact path="*" element={<Error />}></Route>
-      </Routes>
-      {/* <Footer /> */}
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        {/* <MainNav /> */}
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          {/* <Route exact path="/employee-list" element={<EmployeeList />}></Route> */}
+          <Route exact path="*" element={<Error />}></Route>
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
+    </ThemeProvider>
   );
 }
 
