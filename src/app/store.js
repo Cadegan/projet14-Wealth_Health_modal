b266f54cafcf7f34e06b2a4900b8dbd8b9e4d7a1
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-12-29 10:17:41 +0100

    getDefaultMiddleware "deprecated" removed

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import employeeReducer from "../slices/employeeSlice";
 
 /* Creating a store with the reducer. */
@@ -6,7 +6,8 @@ export default configureStore({
   reducer: {
     employee: employeeReducer,
   },
-  middleware: getDefaultMiddleware({
-    serializableCheck: false,
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
