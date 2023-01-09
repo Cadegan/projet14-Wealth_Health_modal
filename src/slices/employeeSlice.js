9a47fc6693d3060110e7764d5eeda6894931baae
Cadegan <91974649+Cadegan@users.noreply.github.com>
2023-01-09 05:22:50 +0100

    EmployeeSlice correction

--- src/slices/employeeSlice.js
+++ src/slices/employeeSlice.js
@@ -3,9 +3,8 @@ import initialEmployeeList from "../dataMoked.json";
 
 /* V3 */
 const initialState = {
-  employeesArray: JSON.parse(localStorage.getItem("employees")) || [
-    ...initialEmployeeList,
-  ],
+  employeesArray:
+    JSON.parse(localStorage.getItem("employees")) || initialEmployeeList,
 };
 
 export const employeeSlice = createSlice({
@@ -20,45 +19,3 @@ export const employeeSlice = createSlice({
 
 export const { addEmployee } = employeeSlice.actions;
 export default employeeSlice.reducer;
-
-/* V2 */
-// const employee = createSlice({
-//   name: "addEmployee",
-//   initialState: {
-//     employeeList: actionType.getEmployeeList(),
-//   },
-//   reducers: {
-//     addEmployee: (state, action) => {
-//       // state.employeeList.ushift(action.payload);
-//       actionType.setEmployeeList(state.employeeList);
-//     },
-//   },
-// });
-
-// const employeeAction = employee.actions;
-// const employeeReducer = employee.reducer;
-
-// export { employeeAction, employeeReducer };
-
-////////////////////////////////////////////////////////////////////////
-
-// const {
-//   // register,
-//   handleSubmit,
-//   reset,
-//   control,
-//   formState: { isValid },
-// } = useForm({
-//   criteriaMode: "all",
-//   mode: "onChange",
-// });
-// const employees = JSON.parse(localStorage.getItem("employees")) || [];
-// const onSubmit = (data) => {
-//   employees.push(data);
-//   localStorage.setItem("employees", JSON.stringify(employees));
-//   setShowModal(true);
-//   reset();
-//   // alert(JSON.stringify(data, null, 2));
-// };
-
-// console.log("Submit result", employees);
